f78a3c17ca544d8056a5f7824d5dda3d47c3c26d
Onur <abdullahonur18@gmail.com>
2021-08-26 16:12:56 +0300

    Initial Commit

---
+++ akillikonumteknolojileri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  akillikonumteknolojileri
+//
+//  Created by Abdullah onur Şimşek on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
