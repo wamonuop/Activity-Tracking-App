f2bc8c836b44592c661d14d0b6b36a0f231b45cc
Onur <abdullahonur18@gmail.com>
2021-08-31 18:28:16 +0300

    Finished Project

--- akillikonumteknolojileri/AppDelegate.swift
+++ akillikonumteknolojileri/AppDelegate.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import CoreData
-import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyDXn_hk_5EfhBUn1XUEYybcMx3sLa2ario")
         return true
     }
 
